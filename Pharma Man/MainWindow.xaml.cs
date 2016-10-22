2e05f39449645082bc6807419116611350e4e978
er4zox <deejayalexxd@gmail.com>
2016-10-22 20:48:38 +0200

    Switched to C# WPF

---
+++ Pharma Man/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pharma_Man
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        Besuchsplan bp;
+        BesuchsplanErfassen bpe;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            bp = new Besuchsplan();
+            bpe = new BesuchsplanErfassen();
+
+            frame.NavigationService.Navigate(bp);
+        }
+
+        private void btn_next_Click(object sender, RoutedEventArgs e)
+        {
+            if (frame.NavigationService.CanGoForward)
+            {
+                frame.NavigationService.GoForward();
+            }
+        }
+
+        private void btn_prev_Click(object sender, RoutedEventArgs e)
+        {
+            if (frame.NavigationService.CanGoBack)
+            {
+                frame.NavigationService.GoBack();
+            }
+        }
+
+        private void btn_Besuchsplan_Click(object sender, RoutedEventArgs e)
+        {
+            frame.NavigationService.Navigate(bp);
+        }
+
+        private void btn_BesuchErfassen_Click(object sender, RoutedEventArgs e)
+        {
+            frame.NavigationService.Navigate(bpe);
+        }
+    }
+}
---
+++ Pharma Man/Tagesplan.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pharma_Man
+{
+    /// <summary>
+    /// Interaktionslogik für Tagesplan.xaml
+    /// </summary>
+    public partial class Tagesplan : Page
+    {
+        public Tagesplan(DateTime date)
+        {
+            InitializeComponent();
+            lbl_date.Content = "für den " + date.ToShortDateString() + ".";
+
+
+            ListBoxItem itm = new ListBoxItem();
+            itm.Content = "some text";
+
+            docList.Items.Add(itm);
+
+        }
+    }
+}
